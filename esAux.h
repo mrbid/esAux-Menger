96d4ed621bdaf2c543b92695143b6435948ee750
esAux <75986423+esAux@users.noreply.github.com>
2020-12-21 09:18:40 +0000

    Update esAux.h

--- esAux.h
+++ esAux.h
@@ -312,28 +312,28 @@ void esMatrixCopy(ESMatrix* r, const ESMatrix* v)
 void esMatrixMultiply(ESMatrix *result, const ESMatrix *srcA, const ESMatrix *srcB)
 {
     ESMatrix tmp;
-	for(int i=0; i<4; i++)
-	{
-		tmp.m[i][0] =	(srcA->m[i][0] * srcB->m[0][0]) +
-						(srcA->m[i][1] * srcB->m[1][0]) +
-						(srcA->m[i][2] * srcB->m[2][0]) +
-						(srcA->m[i][3] * srcB->m[3][0]) ;
+    for(int i=0; i<4; i++)
+    {
+        tmp.m[i][0] =	(srcA->m[i][0] * srcB->m[0][0]) +
+                        (srcA->m[i][1] * srcB->m[1][0]) +
+                        (srcA->m[i][2] * srcB->m[2][0]) +
+                        (srcA->m[i][3] * srcB->m[3][0]) ;
 
-		tmp.m[i][1] =	(srcA->m[i][0] * srcB->m[0][1]) + 
-						(srcA->m[i][1] * srcB->m[1][1]) +
-						(srcA->m[i][2] * srcB->m[2][1]) +
-						(srcA->m[i][3] * srcB->m[3][1]) ;
+        tmp.m[i][1] =	(srcA->m[i][0] * srcB->m[0][1]) + 
+                        (srcA->m[i][1] * srcB->m[1][1]) +
+                        (srcA->m[i][2] * srcB->m[2][1]) +
+                        (srcA->m[i][3] * srcB->m[3][1]) ;
 
-		tmp.m[i][2] =	(srcA->m[i][0] * srcB->m[0][2]) + 
-						(srcA->m[i][1] * srcB->m[1][2]) +
-						(srcA->m[i][2] * srcB->m[2][2]) +
-						(srcA->m[i][3] * srcB->m[3][2]) ;
+        tmp.m[i][2] =	(srcA->m[i][0] * srcB->m[0][2]) + 
+                        (srcA->m[i][1] * srcB->m[1][2]) +
+                        (srcA->m[i][2] * srcB->m[2][2]) +
+                        (srcA->m[i][3] * srcB->m[3][2]) ;
 
-		tmp.m[i][3] =	(srcA->m[i][0] * srcB->m[0][3]) + 
-						(srcA->m[i][1] * srcB->m[1][3]) +
-						(srcA->m[i][2] * srcB->m[2][3]) +
-						(srcA->m[i][3] * srcB->m[3][3]) ;
-	}
+        tmp.m[i][3] =	(srcA->m[i][0] * srcB->m[0][3]) + 
+                        (srcA->m[i][1] * srcB->m[1][3]) +
+                        (srcA->m[i][2] * srcB->m[2][3]) +
+                        (srcA->m[i][3] * srcB->m[3][3]) ;
+    }
     memcpy(result, &tmp, sizeof(ESMatrix));
 }
 
@@ -551,89 +551,89 @@ void esLookAt(ESMatrix *result, const GLfloat origin_x, const GLfloat origin_y, 
 
 void esCramerInvert(GLfloat *dst, const GLfloat *mat)
 {
-	GLfloat tmp[12]; /* temp array for pairs */
-	GLfloat src[16]; /* array of transpose source matrix */
-	GLfloat det; /* determinant */
+    GLfloat tmp[12]; /* temp array for pairs */
+    GLfloat src[16]; /* array of transpose source matrix */
+    GLfloat det; /* determinant */
 
-	/* transpose matrix */
-	for(int i = 0; i < 4; i++)
-	{
-		src[i] = mat[i*4];
-		src[i + 4] = mat[i*4 + 1];
-		src[i + 8] = mat[i*4 + 2];
-		src[i + 12] = mat[i*4 + 3];
-	}
+    /* transpose matrix */
+    for(int i = 0; i < 4; i++)
+    {
+	    src[i] = mat[i*4];
+	    src[i + 4] = mat[i*4 + 1];
+	    src[i + 8] = mat[i*4 + 2];
+	    src[i + 12] = mat[i*4 + 3];
+    }
 
-	/* calculate pairs for first 8 elements (cofactors) */
-	tmp[0] = src[10] * src[15];
-	tmp[1] = src[11] * src[14];
-	tmp[2] = src[9] * src[15];
-	tmp[3] = src[11] * src[13];
-	tmp[4] = src[9] * src[14];
-	tmp[5] = src[10] * src[13];
-	tmp[6] = src[8] * src[15];
-	tmp[7] = src[11] * src[12];
-	tmp[8] = src[8] * src[14];
-	tmp[9] = src[10] * src[12];
-	tmp[10] = src[8] * src[13];
-	tmp[11] = src[9] * src[12];
+    /* calculate pairs for first 8 elements (cofactors) */
+    tmp[0] = src[10] * src[15];
+    tmp[1] = src[11] * src[14];
+    tmp[2] = src[9] * src[15];
+    tmp[3] = src[11] * src[13];
+    tmp[4] = src[9] * src[14];
+    tmp[5] = src[10] * src[13];
+    tmp[6] = src[8] * src[15];
+    tmp[7] = src[11] * src[12];
+    tmp[8] = src[8] * src[14];
+    tmp[9] = src[10] * src[12];
+    tmp[10] = src[8] * src[13];
+    tmp[11] = src[9] * src[12];
 
-	/* calculate first 8 elements (cofactors) */
-	dst[0] = tmp[0]*src[5] + tmp[3]*src[6] + tmp[4]*src[7];
-	dst[0] -= tmp[1]*src[5] + tmp[2]*src[6] + tmp[5]*src[7];
-	dst[1] = tmp[1]*src[4] + tmp[6]*src[6] + tmp[9]*src[7];
-	dst[1] -= tmp[0]*src[4] + tmp[7]*src[6] + tmp[8]*src[7];
-	dst[2] = tmp[2]*src[4] + tmp[7]*src[5] + tmp[10]*src[7];
-	dst[2] -= tmp[3]*src[4] + tmp[6]*src[5] + tmp[11]*src[7];
-	dst[3] = tmp[5]*src[4] + tmp[8]*src[5] + tmp[11]*src[6];
-	dst[3] -= tmp[4]*src[4] + tmp[9]*src[5] + tmp[10]*src[6];
-	dst[4] = tmp[1]*src[1] + tmp[2]*src[2] + tmp[5]*src[3];
-	dst[4] -= tmp[0]*src[1] + tmp[3]*src[2] + tmp[4]*src[3];
-	dst[5] = tmp[0]*src[0] + tmp[7]*src[2] + tmp[8]*src[3];
-	dst[5] -= tmp[1]*src[0] + tmp[6]*src[2] + tmp[9]*src[3];
-	dst[6] = tmp[3]*src[0] + tmp[6]*src[1] + tmp[11]*src[3];
-	dst[6] -= tmp[2]*src[0] + tmp[7]*src[1] + tmp[10]*src[3];
-	dst[7] = tmp[4]*src[0] + tmp[9]*src[1] + tmp[10]*src[2];
-	dst[7] -= tmp[5]*src[0] + tmp[8]*src[1] + tmp[11]*src[2];
+    /* calculate first 8 elements (cofactors) */
+    dst[0] = tmp[0]*src[5] + tmp[3]*src[6] + tmp[4]*src[7];
+    dst[0] -= tmp[1]*src[5] + tmp[2]*src[6] + tmp[5]*src[7];
+    dst[1] = tmp[1]*src[4] + tmp[6]*src[6] + tmp[9]*src[7];
+    dst[1] -= tmp[0]*src[4] + tmp[7]*src[6] + tmp[8]*src[7];
+    dst[2] = tmp[2]*src[4] + tmp[7]*src[5] + tmp[10]*src[7];
+    dst[2] -= tmp[3]*src[4] + tmp[6]*src[5] + tmp[11]*src[7];
+    dst[3] = tmp[5]*src[4] + tmp[8]*src[5] + tmp[11]*src[6];
+    dst[3] -= tmp[4]*src[4] + tmp[9]*src[5] + tmp[10]*src[6];
+    dst[4] = tmp[1]*src[1] + tmp[2]*src[2] + tmp[5]*src[3];
+    dst[4] -= tmp[0]*src[1] + tmp[3]*src[2] + tmp[4]*src[3];
+    dst[5] = tmp[0]*src[0] + tmp[7]*src[2] + tmp[8]*src[3];
+    dst[5] -= tmp[1]*src[0] + tmp[6]*src[2] + tmp[9]*src[3];
+    dst[6] = tmp[3]*src[0] + tmp[6]*src[1] + tmp[11]*src[3];
+    dst[6] -= tmp[2]*src[0] + tmp[7]*src[1] + tmp[10]*src[3];
+    dst[7] = tmp[4]*src[0] + tmp[9]*src[1] + tmp[10]*src[2];
+    dst[7] -= tmp[5]*src[0] + tmp[8]*src[1] + tmp[11]*src[2];
 
-	/* calculate pairs for second 8 elements (cofactors) */
-	tmp[0] = src[2]*src[7];
-	tmp[1] = src[3]*src[6];
-	tmp[2] = src[1]*src[7];
-	tmp[3] = src[3]*src[5];
-	tmp[4] = src[1]*src[6];
-	tmp[5] = src[2]*src[5];
-	tmp[6] = src[0]*src[7];
-	tmp[7] = src[3]*src[4];
-	tmp[8] = src[0]*src[6];
-	tmp[9] = src[2]*src[4];
-	tmp[10] = src[0]*src[5];
-	tmp[11] = src[1]*src[4];
+    /* calculate pairs for second 8 elements (cofactors) */
+    tmp[0] = src[2]*src[7];
+    tmp[1] = src[3]*src[6];
+    tmp[2] = src[1]*src[7];
+    tmp[3] = src[3]*src[5];
+    tmp[4] = src[1]*src[6];
+    tmp[5] = src[2]*src[5];
+    tmp[6] = src[0]*src[7];
+    tmp[7] = src[3]*src[4];
+    tmp[8] = src[0]*src[6];
+    tmp[9] = src[2]*src[4];
+    tmp[10] = src[0]*src[5];
+    tmp[11] = src[1]*src[4];
 
-	/* calculate second 8 elements (cofactors) */
-	dst[8] = tmp[0]*src[13] + tmp[3]*src[14] + tmp[4]*src[15];
-	dst[8] -= tmp[1]*src[13] + tmp[2]*src[14] + tmp[5]*src[15];
-	dst[9] = tmp[1]*src[12] + tmp[6]*src[14] + tmp[9]*src[15];
-	dst[9] -= tmp[0]*src[12] + tmp[7]*src[14] + tmp[8]*src[15];
-	dst[10] = tmp[2]*src[12] + tmp[7]*src[13] + tmp[10]*src[15];
-	dst[10]-= tmp[3]*src[12] + tmp[6]*src[13] + tmp[11]*src[15];
-	dst[11] = tmp[5]*src[12] + tmp[8]*src[13] + tmp[11]*src[14];
-	dst[11]-= tmp[4]*src[12] + tmp[9]*src[13] + tmp[10]*src[14];
-	dst[12] = tmp[2]*src[10] + tmp[5]*src[11] + tmp[1]*src[9];
-	dst[12]-= tmp[4]*src[11] + tmp[0]*src[9] + tmp[3]*src[10];
-	dst[13] = tmp[8]*src[11] + tmp[0]*src[8] + tmp[7]*src[10];
-	dst[13]-= tmp[6]*src[10] + tmp[9]*src[11] + tmp[1]*src[8];
-	dst[14] = tmp[6]*src[9] + tmp[11]*src[11] + tmp[3]*src[8];
-	dst[14]-= tmp[10]*src[11] + tmp[2]*src[8] + tmp[7]*src[9];
-	dst[15] = tmp[10]*src[10] + tmp[4]*src[8] + tmp[9]*src[9];
-	dst[15]-= tmp[8]*src[9] + tmp[11]*src[10] + tmp[5]*src[8];
+    /* calculate second 8 elements (cofactors) */
+    dst[8] = tmp[0]*src[13] + tmp[3]*src[14] + tmp[4]*src[15];
+    dst[8] -= tmp[1]*src[13] + tmp[2]*src[14] + tmp[5]*src[15];
+    dst[9] = tmp[1]*src[12] + tmp[6]*src[14] + tmp[9]*src[15];
+    dst[9] -= tmp[0]*src[12] + tmp[7]*src[14] + tmp[8]*src[15];
+    dst[10] = tmp[2]*src[12] + tmp[7]*src[13] + tmp[10]*src[15];
+    dst[10]-= tmp[3]*src[12] + tmp[6]*src[13] + tmp[11]*src[15];
+    dst[11] = tmp[5]*src[12] + tmp[8]*src[13] + tmp[11]*src[14];
+    dst[11]-= tmp[4]*src[12] + tmp[9]*src[13] + tmp[10]*src[14];
+    dst[12] = tmp[2]*src[10] + tmp[5]*src[11] + tmp[1]*src[9];
+    dst[12]-= tmp[4]*src[11] + tmp[0]*src[9] + tmp[3]*src[10];
+    dst[13] = tmp[8]*src[11] + tmp[0]*src[8] + tmp[7]*src[10];
+    dst[13]-= tmp[6]*src[10] + tmp[9]*src[11] + tmp[1]*src[8];
+    dst[14] = tmp[6]*src[9] + tmp[11]*src[11] + tmp[3]*src[8];
+    dst[14]-= tmp[10]*src[11] + tmp[2]*src[8] + tmp[7]*src[9];
+    dst[15] = tmp[10]*src[10] + tmp[4]*src[8] + tmp[9]*src[9];
+    dst[15]-= tmp[8]*src[9] + tmp[11]*src[10] + tmp[5]*src[8];
 
-	/* calculate determinant */
-	det = src[0]*dst[0]+src[1]*dst[1]+src[2]*dst[2]+src[3]*dst[3];
+    /* calculate determinant */
+    det = src[0]*dst[0]+src[1]*dst[1]+src[2]*dst[2]+src[3]*dst[3];
 
-	/* calculate matrix inverse */
-	det = 1.0f/det;
-	for(int j = 0; j < 16; j++){dst[j] *= det;}
+    /* calculate matrix inverse */
+    det = 1.0f/det;
+    for(int j = 0; j < 16; j++){dst[j] *= det;}
 }
 
 void esTranspose(ESMatrix *r, const ESMatrix* m)
